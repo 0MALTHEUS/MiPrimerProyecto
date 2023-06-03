7944fc8022fc47ad9df3eb3852ee24f81bdca83f
0MALTHEUS <124302434+0MALTHEUS@users.noreply.github.com>
2023-06-03 18:23:23 -0400

    primer seguimiento

---
+++ index.js
@@ -0,0 +1,10 @@
+
+document.getElementById("imagen").src="https://static1-es.millenium.gg/articles/0/36/52/0/@/170487-proximos-campeones-lol-article_image_t-1.jpg"
+function CambiarImagen1(){
+  return document.getElementById("imagen").src="https://th.bing.com/th/id/OIP.Hknx-OrdB5wUhjct7LPQXAHaEK?pid=ImgDet&rs=1"
+}
+function CambiarImagen2(){
+  document.getElementById("imagen").src="https://nexus.leagueoflegends.com/wp-content/uploads/2017/06/yasuoar-min.jpg"
+}
+    
+
